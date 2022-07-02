7b548f2f7c8aadc522cf2cfc34c6e1f996347a71
arlprogramming <arl.code03@gmail.com>
2022-07-02 23:59:55 +0700

    update

--- src/components/javacript/content/numbers/Numbers.jsx
+++ src/components/javacript/content/numbers/Numbers.jsx
@@ -5,7 +5,7 @@ const Numbers = () => {
   const {numbers} = useParams();
   return (
     <div>
-        <h2 className="tittle__content">JAVASCRIPT NUMBERS</h2>
+        <h2 className="tittle__content">JAVASCRIPT {numbers}</h2>
         <div className="materi__content">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur debitis aut aperiam minus tempora et in nemo atque quas voluptas, rem accusamus quod reprehenderit nisi pariatur deleniti non ratione? Molestias?

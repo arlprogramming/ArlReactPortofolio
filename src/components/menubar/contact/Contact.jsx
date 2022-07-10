5016784b860680155c2f1687e983f7a33d80c582
arlprogramming <arl.code03@gmail.com>
2022-07-11 06:33:41 +0700

    contact

--- src/components/menubar/contact/Contact.jsx
+++ src/components/menubar/contact/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../../menubar/contact/contact.css'
 
 const Contact = () => {
   return (
-    <div>
-      MyContact
+    <div className='container__contact'>
+      <h1>Under Maintenance</h1> 
     </div>
   )
 }
